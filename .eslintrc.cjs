4bdd54d3e942890570fca2865c9a129eca1da46d
linhaotxl <602523587@qq.com>
2022-10-23 13:44:35 +0800

    chore: eslint,prettier,lint-staged,commitlint

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+    browser: false,
+  },
+
+  globals: {},
+
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+
+  plugins: ['@typescript-eslint'],
+
+  parser: '@typescript-eslint/parser',
+  parserOptions: {},
+
+  rules: {
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-types': 'off',
+  },
+}
---
+++ src/.eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: false,
+    browser: true,
+    'vue/setup-compiler-macros': true,
+  },
+
+  globals: {
+    uni: true,
+    wx: true,
+    UniNamespace: true,
+    getCurrentPages: true,
+  },
+
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+
+  plugins: ['vue', '@typescript-eslint'],
+
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+
+  rules: {
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-types': 'off',
+  },
+}
