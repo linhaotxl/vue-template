d574b26a542e889547692e923362b3d171e04f22
linhaotxl <602523587@qq.com>
2023-01-31 17:07:17 +0800

    feat: 增加检测数据方法

--- packages/webpack/src/utils/is.ts
+++ packages/webpack/src/utils/is.ts
@@ -1,2 +1,11 @@
 export const isString = (value: unknown): value is string =>
   typeof value === 'string'
+
+export const isFunction = (value: unknown): value is Function =>
+  typeof value === 'function'
+
+export const isPromise = (value: unknown): value is Promise<unknown> =>
+  isObject(value) && isFunction(value.then) && isFunction(value.catch)
+
+export const isObject = (value: unknown): value is Record<string, any> =>
+  typeof value === 'object' && value !== null
