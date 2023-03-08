fe7342a23b241f89051f2d93d115997c52a5c49b
linhaotxl <602523587@qq.com>
2023-03-08 16:06:08 +0800

    feat: 增加valueType类型

--- packages/core/src/components/ProForm/valueTypes.tsx
+++ packages/core/src/components/ProForm/valueTypes.tsx
@@ -1,10 +1,49 @@
-import { ElInput, ElOption, ElSelect } from 'element-plus'
+import {
+  ElCheckbox,
+  ElCheckboxButton,
+  ElCheckboxGroup,
+  ElColorPicker,
+  ElDatePicker,
+  ElInput,
+  ElInputNumber,
+  ElOption,
+  ElRadio,
+  ElRadioButton,
+  ElRadioGroup,
+  ElRate,
+  ElSelect,
+  ElSlider,
+  ElSwitch,
+  ElTimePicker,
+  ElTimeSelect,
+} from 'element-plus'
 import { h } from 'vue'
 
 export const enum ValueTypes {
   Text = 'text',
+  Textarea = 'textarea',
+  Digit = 'digit',
   Password = 'password',
   Select = 'select',
+  Checkbox = 'checkbox',
+  CheckboxButton = 'checkbox-button',
+  Radio = 'radio',
+  RadioButton = 'radio-button',
+  Color = 'color',
+  Date = 'date',
+  Dates = 'dates',
+  DateWeek = 'date-week',
+  DateMonth = 'date-month',
+  DateYear = 'date-year',
+  DateRange = 'date-range',
+  DateMonthRange = 'date-month-range',
+  DateTime = 'date-time',
+  DateTimeRange = 'date-time-range',
+  Time = 'time',
+  TimeSelect = 'time-select',
+  Rate = 'rate',
+  Slider = 'slider',
+  Switch = 'switch',
 }
 
 type RenderValueTypeParams = {
@@ -22,6 +61,18 @@ export const valueTypeMap: Record<ValueTypes, RenderValueType> = {
     <ElInput {...props.fieldProps} v-model={formState[props.prop]} />
   ),
 
+  [ValueTypes.Textarea]: ({ formState, props }) => (
+    <ElInput
+      {...props.fieldProps}
+      type="textarea"
+      v-model={formState[props.prop]}
+    />
+  ),
+
+  [ValueTypes.Digit]: ({ formState, props }) => (
+    <ElInputNumber {...props.fieldProps} v-model={formState[props.prop]} />
+  ),
+
   [ValueTypes.Password]: ({ formState, props }) => (
     <ElInput
       {...props.fieldProps}
@@ -37,10 +88,144 @@ export const valueTypeMap: Record<ValueTypes, RenderValueType> = {
       ))}
     </ElSelect>
   ),
-}
 
-export const valueType2ComponentName: Record<string, string> = {
-  [ValueTypes.Text]: 'el-input',
-  [ValueTypes.Password]: 'el-input',
-  [ValueTypes.Select]: 'el-select',
+  [ValueTypes.Checkbox]: ({ props, formState }) => (
+    <ElCheckboxGroup {...props.fieldProps} v-model={formState[props.prop]}>
+      {props.valueEnum?.map(item => (
+        <ElCheckbox label={item.value}>{item.label}</ElCheckbox>
+      ))}
+    </ElCheckboxGroup>
+  ),
+
+  [ValueTypes.CheckboxButton]: ({ props, formState }) => (
+    <ElCheckboxGroup {...props.fieldProps} v-model={formState[props.prop]}>
+      {props.valueEnum?.map(item => (
+        <ElCheckboxButton label={item.value}>{item.label}</ElCheckboxButton>
+      ))}
+    </ElCheckboxGroup>
+  ),
+
+  [ValueTypes.Radio]: ({ props, formState }) => (
+    <ElRadioGroup {...props.fieldProps} v-model={formState[props.prop]}>
+      {props.valueEnum?.map(item => (
+        <ElRadio label={item.value}>{item.label}</ElRadio>
+      ))}
+    </ElRadioGroup>
+  ),
+
+  [ValueTypes.RadioButton]: ({ props, formState }) => (
+    <ElRadioGroup {...props.fieldProps} v-model={formState[props.prop]}>
+      {props.valueEnum?.map(item => (
+        <ElRadioButton label={item.value}>{item.label}</ElRadioButton>
+      ))}
+    </ElRadioGroup>
+  ),
+
+  [ValueTypes.Color]: ({ props, formState }) => (
+    <ElColorPicker {...props.fieldProps} v-model={formState[props.prop]} />
+  ),
+
+  [ValueTypes.Date]: ({ formState, props }) => (
+    <ElDatePicker
+      valueFormat="YYYY-MM-DD"
+      {...props.fieldProps}
+      v-model={formState[props.prop]}
+    />
+  ),
+
+  [ValueTypes.DateMonth]: ({ formState, props }) => (
+    <ElDatePicker
+      valueFormat="YYYY-MM-DD"
+      {...props.fieldProps}
+      type="month"
+      v-model={formState[props.prop]}
+    />
+  ),
+
+  [ValueTypes.DateMonthRange]: ({ formState, props }) => (
+    <ElDatePicker
+      valueFormat="YYYY-MM"
+      {...props.fieldProps}
+      type="monthrange"
+      v-model={formState[props.prop]}
+    />
+  ),
+
+  [ValueTypes.DateRange]: ({ formState, props }) => (
+    <ElDatePicker
+      valueFormat="YYYY-MM-DD"
+      {...props.fieldProps}
+      type="daterange"
+      v-model={formState[props.prop]}
+    />
+  ),
+
+  [ValueTypes.DateWeek]: ({ formState, props }) => (
+    <ElDatePicker
+      valueFormat="YYYY-MM-DD"
+      {...props.fieldProps}
+      type="week"
+      v-model={formState[props.prop]}
+    />
+  ),
+
+  [ValueTypes.DateYear]: ({ formState, props }) => (
+    <ElDatePicker
+      valueFormat="YYYY"
+      {...props.fieldProps}
+      type="year"
+      v-model={formState[props.prop]}
+    />
+  ),
+
+  [ValueTypes.Dates]: ({ formState, props }) => (
+    <ElDatePicker
+      valueFormat="YYYY-MM-DD"
+      {...props.fieldProps}
+      type="dates"
+      v-model={formState[props.prop]}
+    />
+  ),
+
+  [ValueTypes.DateTime]: ({ formState, props }) => (
+    <ElDatePicker
+      valueFormat="YYYY-MM-DD HH:mm:ss"
+      {...props.fieldProps}
+      type="datetime"
+      v-model={formState[props.prop]}
+    />
+  ),
+
+  [ValueTypes.DateTimeRange]: ({ formState, props }) => (
+    <ElDatePicker
+      valueFormat="YYYY-MM-DD HH:mm:ss"
+      {...props.fieldProps}
+      type="datetimerange"
+      v-model={formState[props.prop]}
+    />
+  ),
+
+  [ValueTypes.Time]: ({ formState, props }) => (
+    <ElTimePicker
+      valueFormat="HH:mm:ss"
+      {...props.fieldProps}
+      v-model={formState[props.prop]}
+    />
+  ),
+
+  [ValueTypes.TimeSelect]: ({ formState, props }) => (
+    <ElTimeSelect {...props.fieldProps} v-model={formState[props.prop]} />
+  ),
+
+  [ValueTypes.Rate]: ({ formState, props }) => (
+    <ElRate {...props.fieldProps} v-model={formState[props.prop]} />
+  ),
+
+  [ValueTypes.Slider]: ({ formState, props }) => (
+    <ElSlider {...props.fieldProps} v-model={formState[props.prop]} />
+  ),
+
+  [ValueTypes.Switch]: ({ formState, props }) => (
+    <ElSwitch {...props.fieldProps} v-model={formState[props.prop]} />
+  ),
 }
---
+++ packages/playground/src/pages/pro-form-value-type.vue
@@ -0,0 +1,246 @@
+<template>
+  <!-- <el-space direction="vertical" alignment="flex-start"> -->
+  <pro-form
+    label-width="100px"
+    :render-tools="true"
+    @finish="handleSubmit"
+    @finish-faild="handleFinishFailed"
+  >
+    <pro-form-item
+      label="普通输入框"
+      prop="userName"
+      label-width="100px"
+      required
+      value-type="text"
+      :field-props="{
+        placeholder: '请输入用户名',
+        maxlength: 20,
+        minlength: 10,
+        clearable: true,
+      }"
+    />
+
+    <pro-form-item
+      label="文本域"
+      prop="textarea"
+      label-width="100px"
+      required
+      value-type="textarea"
+      :field-props="{
+        placeholder: '请输入文本内容',
+      }"
+    />
+
+    <pro-form-item
+      label="密码"
+      prop="password"
+      required
+      value-type="password"
+      :field-props="{ placeholder: '请输入密码' }"
+    />
+
+    <pro-form-item
+      label="下拉框"
+      prop="sex"
+      required
+      value-type="select"
+      :value-enum="[
+        { label: '男', value: 1 },
+        { label: '女', value: 2 },
+        { label: '自定义', value: 3 },
+      ]"
+      :field-props="{ placeholder: '请选择性别' }"
+    >
+    </pro-form-item>
+
+    <pro-form-item
+      label="复选框"
+      prop="checkbox"
+      required
+      value-type="checkbox"
+      :value-enum="[
+        { label: '男', value: 1 },
+        { label: '女', value: 2 },
+        { label: '自定义', value: 3 },
+      ]"
+      :field-props="{ min: 0, max: 2 }"
+    />
+
+    <pro-form-item
+      label="按钮复选框"
+      prop="checkboxButton"
+      required
+      value-type="checkbox-button"
+      :value-enum="[
+        { label: '男', value: 1 },
+        { label: '女', value: 2 },
+        { label: '自定义', value: 3 },
+      ]"
+      :field-props="{ min: 0, max: 2 }"
+    />
+
+    <pro-form-item
+      label="单选框"
+      prop="radio"
+      required
+      value-type="radio"
+      :value-enum="[
+        { label: '男', value: 1 },
+        { label: '女', value: 2 },
+        { label: '自定义', value: 3 },
+      ]"
+      :field-props="{ disabled: true }"
+    />
+
+    <pro-form-item
+      label="按钮单选框"
+      prop="radioButton"
+      required
+      value-type="radio-button"
+      :value-enum="[
+        { label: '男', value: 1 },
+        { label: '女', value: 2 },
+        { label: '自定义', value: 3 },
+      ]"
+      :field-props="{ textColor: '#f99' }"
+    />
+
+    <pro-form-item
+      label="颜色"
+      prop="color"
+      required
+      value-type="color"
+      :field-props="{ size: 'small' }"
+    />
+
+    <pro-form-item
+      label="日期"
+      prop="date"
+      required
+      value-type="date"
+      :field-props="{
+        valueFormat: 'YYYY-MM-DD HH:mm:ss',
+        onVisibleChange: handleChangeDateVisible,
+      }"
+    />
+
+    <pro-form-item
+      label="周"
+      prop="week"
+      required
+      value-type="date-week"
+      :field-props="{}"
+    />
+
+    <pro-form-item
+      label="月"
+      prop="month"
+      required
+      value-type="date-month"
+      :field-props="{}"
+    />
+
+    <pro-form-item
+      label="年"
+      prop="year"
+      required
+      value-type="date-year"
+      :field-props="{}"
+    />
+
+    <pro-form-item
+      label="多个日期"
+      prop="dates"
+      required
+      value-type="dates"
+      :field-props="{}"
+    />
+
+    <pro-form-item
+      label="日期范围"
+      prop="dateRange"
+      required
+      value-type="date-range"
+      :field-props="{}"
+    />
+
+    <pro-form-item
+      label="月范围"
+      prop="monthRange"
+      required
+      value-type="date-month-range"
+      :field-props="{}"
+    />
+
+    <pro-form-item
+      label="日期时间"
+      prop="dateTime"
+      required
+      value-type="date-time"
+      :field-props="{}"
+    />
+
+    <pro-form-item
+      label="日期时间范围"
+      prop="dateTimeRange"
+      required
+      value-type="date-time-range"
+      :field-props="{}"
+    />
+
+    <pro-form-item
+      label="时间"
+      prop="time"
+      required
+      value-type="time"
+      :field-props="{}"
+    />
+
+    <pro-form-item
+      label="选择时间"
+      prop="timeSelect"
+      required
+      value-type="time-select"
+      :field-props="{}"
+    />
+
+    <pro-form-item
+      label="滑块"
+      prop="slider"
+      required
+      value-type="slider"
+      :field-props="{}"
+    />
+
+    <pro-form-item
+      label="开关"
+      prop="switch"
+      required
+      value-type="switch"
+      :field-props="{}"
+    />
+
+    <pro-form-item
+      label="评分"
+      prop="rate"
+      required
+      value-type="rate"
+      :field-props="{}"
+    />
+  </pro-form>
+  <!-- </el-space> -->
+</template>
+
+<script lang="ts" setup name="BasicPage">
+const handleSubmit = (values: unknown) => {
+  console.log('submit: ', values)
+}
+
+const handleFinishFailed = ({ values, errorFields }: any) => {
+  console.log('failed: ', values, errorFields)
+}
+
+const handleChangeDateVisible = (visible: boolean) => {
+  console.log('日期窗打开状态: ', visible)
+}
+</script>
