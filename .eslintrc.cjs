ab5946fddef708377431242233bf8c6d79a39824
linhaotxl <602523587@qq.com>
2023-03-10 11:31:09 +0800

    chore: 依赖安装,eslint,ts配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -55,6 +55,8 @@ module.exports = {
         'no-undef': 'error',
         'vue/multi-word-component-names': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: 'h|Fragment' }]
       },
     },
   ],
