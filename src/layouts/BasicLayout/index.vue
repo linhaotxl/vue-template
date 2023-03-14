fa64aaa59f6697f5bedc5d4af318cc1dce688bad
linhaotxl <602523587@qq.com>
2023-03-14 17:52:21 +0800

    feat: 使用el-scrollbar实现滚动条

--- src/layouts/BasicLayout/index.vue
+++ src/layouts/BasicLayout/index.vue
@@ -1,22 +1,26 @@
 <template>
-  <el-container class="overflow-auto">
+  <el-container class="overflow-auto h-full">
     <el-header> Header </el-header>
 
     <el-container class="overflow-auto">
       <el-aside width="var(--aside-width)">
-        <el-menu
-          class="h-full"
-          router
-          :default-openeds="defaultOpeneds"
-          :default-active="defaultActive"
-        >
-          <recursion-menu :menus="menus" />
-        </el-menu>
+        <el-scrollbar view-class="h-full">
+          <el-menu
+            class="h-full"
+            router
+            :default-openeds="defaultOpeneds"
+            :default-active="defaultActive"
+          >
+            <recursion-menu :menus="menus" />
+          </el-menu>
+        </el-scrollbar>
       </el-aside>
 
-      <el-main class="overflow-auto">
-        <router-view />
-      </el-main>
+      <el-scrollbar class="w-full" view-class="h-full">
+        <el-main class="h-full !overflow-visible">
+          <router-view />
+        </el-main>
+      </el-scrollbar>
     </el-container>
   </el-container>
 </template>
